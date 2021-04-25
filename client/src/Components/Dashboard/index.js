2eb8bd3e5c5c04f992267efe92f8f73e29d7563e
Nilda <nick.ovalles@gmail.com>
2021-04-25 16:53:52 -0600

    <dashboard attempt 1 >

---
+++ client/src/Components/Dashboard/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import Nav from '../Nav';
+import Carousel from '../Carousel';
+
+function Dashboard() {
+
+    return (
+
+        <div>
+            <Nav />
+        </div>
+
+
+          );
+}
+
+export default Dashboard;
---
+++ client/src/dashboard.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Header from '../src/Components/Nav';
+import Hero from './Components/Hero';
+
+function Dashboard() {
+  return (
+    <div>
+
+     {/*  <div>
+        <Hero />
+      </div> */}
+
+      <div>
+      <Hero />
+      </div>
+
+      <div>
+      <Header />
+      </div>
+
+
+    </div>
+    
+  );
+}
+
+export default Dashboard;

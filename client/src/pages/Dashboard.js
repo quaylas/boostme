86196c9a80527213ef74b5c98b7c9c30606671cd
Nilda <nick.ovalles@gmail.com>
2021-04-27 17:24:05 -0600

    <dashboard updated, code is commented out>

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import { useMutation } from '@apollo/react-hooks';
+import { Link } from "react-router-dom";
+/* import { LOGIN } from "../utils/mutations"
+import Auth from "../utils/auth"; */
+
+const Dashboard = () => {
+    return (
+
+        <div>
+
+        </div>
+
+        /* <div>
+        <header class="page-header">
+        <nav>
+            <div id="logo">
+                <h1><a href="index.html">BoostMe</a></h1>
+            </div>
+          <button class="toggle-mob-menu" aria-expanded="false" aria-label="open menu">
+            <svg width="20" height="20" aria-hidden="true">
+              <use xlink:href="#down"></use>
+            </svg>
+          </button>
+          <ul class="admin-menu">
+             <li class="menu-heading">
+              <h3 style="color: aliceblue;">Janele Leathman</h3>
+            <li>
+              <a href="#">
+                <svg>
+                  <use xlink:href="#pages"></use>
+                </svg>
+                <span>My Donations</span>
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <svg>
+                  <use xlink:href="#users"></use>
+                </svg>
+                <span>Benefactors</span>
+              </a>
+            </li>
+            <li>
+              <a href="#0">
+                <svg>
+                  <use xlink:href="#comments"></use>
+                </svg>
+                <span>Account</span>
+              </a>
+            </li>
+            <br />
+                         </li>
+              <button class="button" aria-expanded="#" aria-label="#">
+                <span>Sign Out</span>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <section class="page-content">
+        <section class="search-and-user">
+          <form>
+            <input type="search" placeholder="Begin Typing a Name...">
+            <button type="submit" aria-label="submit form">
+              <svg aria-hidden="true">
+                <use xlink:href="#search"></use>
+              </svg>
+            </button>
+          </form>
+          <div class="admin-profile">
+            <span class="greeting">Hello Janele</span>
+            <div class="notifications">
+              <span class="badge">1</span>
+              <svg>
+                <use xlink:href="#users"></use>
+              </svg>
+            </div>
+          </div>
+        </section>
+        <section class="grid">
+
+        
+          <article style="padding-bottom: 50%;">
+            <form style="padding-left: 5%;">
+                <label for="fname">Name:</label>
+                <input type="text" id="fname" name="fname">
+                <label for="lname">Benefactor:</label>
+                <input type="text" id="lname" name="lname">
+                <label for="lname">Amount:</label>
+                <input type="text" id="lname" name="lname">
+                <br />
+                <input type="submit" value="Donate Now">
+            </form>
+          </article>
+          
+          <article id="my-donations">
+            <h1 style="font-size: 230%; padding-left: 22%; padding-top: 3%;">My Donation <br />History
+            </h1> 
+
+            <form>
+                <input type="submit" value="Donate Now">
+            </form>
+
+          </article>
+          <article></article>
+          <article></article>
+        </section>
+      </section>
+      </div> */
+
+
+       
+    );
+    }
+
+export default Dashboard;

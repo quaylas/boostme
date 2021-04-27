f40111b44d13c3fa67e6d68bae5b91df4fd9fc75
Janele Leatham <janeleleatham@Janeles-MacBook-Air.local>
2021-04-27 13:38:05 -0600

    added map feature

--- client/src/Components/DonationHist/index.js
+++ client/src/Components/DonationHist/index.js
@@ -5,17 +5,40 @@ import { QUERY_USER } from '../../utils/queries';
 
 function DonationHistory() {
     
-    const [userDonations] = useQuery(QUERY_USER)
-    const listItems = userDonations.map((donations) =>
-    <li>{donations}</li>
-    );
+    const { data } = useQuery(QUERY_USER);
+    let user;
+
+    if (data) {
+        user = data.user;
+    }
     
     return (
+        <>
         <div>
-            <ul>
-                {listItems}
-            </ul>
+            {user ? (
+                <>
+                <h2>Donation History</h2>
+        
+                {user.donations.map(({ donationDate, benefactor, amount }, index) => (
+                    <div key={index}>
+                        <li>
+                            {
+                            user.donations.donationDate,
+                            user.donations.benefactor,
+                            user.donations.amount
+                            }
+                        </li>
+
+                    </div>
+
+                )
+
+            )}
+            </>
+        ) : null}
+
         </div>
+        </>
 
     );
 }
